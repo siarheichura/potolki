0ba3ea1e46473ff9a4f3c91116abd412e3f65304
Siarhei Chura <siarhei.chura@connectwise.com>
2022-09-27 00:06:29 +0400

    add basic layout & header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import 'antd/dist/antd.css'
 import { Layout } from './components/_Layout/Layout'
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Layout />
     </div>
   )
--- src/components/_Layout/Header/Header.tsx
+++ src/components/_Layout/Header/Header.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import './Header.scss'
+import { InstagramIcon, TelegramIcon } from '../../Icons/Icons'
+import { PhoneOutlined } from '@ant-design/icons'
 
 export const Header = () => {
-  return <div>HEADER</div>
+  return <header>
+    <div className='logo'></div>
+    <div className='info'>
+      <div className='info_icons'>
+        <TelegramIcon className='info_icon' />
+        <InstagramIcon className='info_icon' />
+      </div>
+      <div>
+        <PhoneOutlined style={{ transform: 'rotateY(180deg)' }} />
+        +375(44)000-00-00
+      </div>
+    </div>
+  </header>
 }
