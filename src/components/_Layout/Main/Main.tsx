1195528e1c15ac7494546a386f6db29a020ed618
Siarhei Chura <siarhei.chura@connectwise.com>
2022-09-29 20:41:52 +0400

    add mobile screen adaptation

--- src/components/_Layout/Main/Main.tsx
+++ src/components/_Layout/Main/Main.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import './Main.scss'
 
 import { Calculator } from '../../Calculator/Calculator'
-import { TextArea } from '../../TextArea/TextArea'
 import { UserForm } from '../../UserForm/UserForm'
 
 export const Main = () => {
   return <main>
     <div className='main-container'>
+      <div style={{ textAlign: 'center', fontWeight: 900 }}>ЗДЕСЬ БУДЕТ КАКОЕ-ТО МИНИМАЛЬНОЕ ОПИСАНИЕ</div>
       <Calculator />
-      <TextArea label='Дополнительныеeee работы (просчитываются индивидуально):' rows={3} />
       <UserForm />
+      <div style={{ textAlign: 'center', fontWeight: 900 }}>
+        ЗДЕСЬ ТОЖЕ ЧТО-ТО БУДЕТ (ТИПА ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ, ОТЗЫВЫ ИЛИ ЕЩЕ ЧЕ)
+      </div>
     </div>
   </main>
 }
