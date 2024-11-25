4b9538941d965290dee2846ad1eb0f308607239f
Nastyshka14 <nastya.korshun.1997@mail.ru>
2024-11-25 18:40:47 +0100

    fix errors and tg chat

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import './MainForm.scss'
 import { Calculator } from './Calculator/Calculator'
 import { UserForm } from './UserForm/UserForm'
-import { Button } from 'antd'
 import { sendMessage } from '../../services/telegram'
+import { Button } from 'antd'
 
 export interface CalculatorValueI {
   square: number
@@ -16,6 +16,18 @@ export interface CalculatorValueI {
 export interface UserFormValueI {
   name: string
   tel: string
+}
+
+export interface UserFormErrorsI {
+  name: string
+  tel: string
+  data: string
+}
+
+export const userFormErrors: UserFormErrorsI = {
+  name: 'Пожалуйста, введите имя',
+  tel: 'Пожалуйста, введите корректный номер',
+  data: 'Пожалуйста, введите данные',
 }
 
 export const MainForm = () => {
@@ -31,33 +43,49 @@ export const MainForm = () => {
     tel: ''
   }
 
-  const [calculatorValue, setCalculatorValue] = useState<CalculatorValueI>(initCalculatorValue)
+  const [calculatorValue, setCalculatorValue] =
+    useState<CalculatorValueI>(initCalculatorValue)
   const [totalAmount, setTotalAmount] = useState<number>(0)
-  const [userFormValue, setUserFormValue] = useState<UserFormValueI>(initUserFormValue)
+  const [userFormValue, setUserFormValue] =
+    useState<UserFormValueI>(initUserFormValue)
+  const [error, setError] = useState<string>('')
 
   const getTotalAmount = () => {
     const { square, lightPoints, cornice, secretCornice } = calculatorValue
-    const amount = square * 9 + lightPoints * 6 + cornice * 9 + secretCornice * 17
+    const amount =
+      square * 9 + lightPoints * 6 + cornice * 9 + secretCornice * 17
     setTotalAmount(amount)
   }
 
   useEffect(() => {
     getTotalAmount()
+    // eslint-disable-next-line
   }, [calculatorValue])
 
   const submitForm = async () => {
     const { name, tel } = userFormValue
     if (name && tel) {
-      const result = { ...calculatorValue, ...userFormValue, totalAmount }
-      await sendMessage(result)
-      setCalculatorValue(initCalculatorValue)
-      setUserFormValue(initUserFormValue)
-      setTotalAmount(0)
+      if (tel.includes('_')) {
+        setError(userFormErrors.tel)
+      } else {
+        const result = { ...calculatorValue, ...userFormValue, totalAmount }
+        await sendMessage(result)
+        setCalculatorValue(initCalculatorValue)
+        setUserFormValue(initUserFormValue)
+        setTotalAmount(0)
+        setError('')
+      }
+    } else if (name && !tel) {
+      setError(userFormErrors.tel)
+    } else if (!name && tel) {
+      setError(userFormErrors.name)
+    } else {
+      setError(userFormErrors.data)
     }
   }
 
   return (
-    <div className='main-form'>
+    <div className="main-form">
       <Calculator
         calculatorValue={calculatorValue}
         setCalculatorValue={setCalculatorValue}
@@ -66,8 +94,14 @@ export const MainForm = () => {
       <UserForm
         userFormValue={userFormValue}
         setUserFormValue={setUserFormValue}
+        error={error}
       />
-      <Button type='primary' onClick={submitForm}>
+      {error.length > 0 && (
+        <div style={{ color: 'red', display: "flex", justifyContent: 'center' }}>
+          {error}
+        </div>
+      )}
+      <Button type="primary" onClick={submitForm}>
         Отправить
       </Button>
     </div>
--- src/components/MainForm/UserForm/UserForm.tsx
+++ src/components/MainForm/UserForm/UserForm.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import './UserForm.scss'
+import { UserFormValueI, userFormErrors } from '../MainForm'
+import InputMask from 'react-input-mask'
 import { Input } from 'antd'
-import { UserFormValueI } from '../MainForm'
 
 interface PropsI {
   userFormValue: UserFormValueI
   setUserFormValue: (value: UserFormValueI) => void
+  error: string
 }
 
-export const UserForm = ({ userFormValue, setUserFormValue }: PropsI) => {
+export const UserForm = ({
+  userFormValue,
+  setUserFormValue,
+  error
+}: PropsI) => {
   const onInputChange = (newValue: UserFormValueI) => setUserFormValue(newValue)
 
   return (
     <>
-      <div className='user-form'>
-        <div className='user-form_label'>
+      <div className="user-form">
+        <div className="user-form_label">
           Чтобы узнать
           <span> точную стоимость </span>
           или
@@ -22,17 +28,36 @@ export const UserForm = ({ userFormValue, setUserFormValue }: PropsI) => {
           оставьте свои контакты и наш менеджер свяжется с Вами
         </div>
 
-        <div className='user-form_inputs'>
+        <div className="user-form_inputs">
           <Input
-            placeholder='Ваше Имя'
+            placeholder="Ваше Имя"
             value={userFormValue.name}
-            onChange={(value) => onInputChange({ ...userFormValue, name: value.target.value })}
+            onChange={(value) =>
+              onInputChange({ ...userFormValue, name: value.target.value })
+            }
+            status={
+              error.length > 0 && error === userFormErrors.name ? 'error' : ''
+            }
           />
-          <Input
-            placeholder='Ваш телефон'
+          <InputMask
+            mask="+375 (99) 999 99 99"
+            placeholder="Ваш телефон"
             value={userFormValue.tel}
-            onChange={(value) => onInputChange({ ...userFormValue, tel: value.target.value })}
-          />
+            onChange={(value) => {
+              onInputChange({ ...userFormValue, tel: value.target.value })
+            }}>
+            {(inputProps) => (
+              <Input
+                {...inputProps}
+                error={true}
+                status={
+                  error.length > 0 && error === userFormErrors.tel
+                    ? 'error'
+                    : ''
+                }
+              />
+            )}
+          </InputMask>
         </div>
       </div>
     </>
