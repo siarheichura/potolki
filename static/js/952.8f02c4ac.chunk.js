43d6b8f1d79a9f5ffbb7d2edb73659704a5c5e05
Nastyshka14 <nastya.korshun.1997@mail.ru>
2025-03-28 07:31:31 +0300

    Updates

---
+++ static/js/952.8f02c4ac.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpotolki=self.webpackChunkpotolki||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.8f02c4ac.chunk.js.map
